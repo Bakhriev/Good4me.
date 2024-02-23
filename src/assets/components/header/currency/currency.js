415da3b68a6545bd81f8d1e9e8b056878627722c
Bakhriev <muradbakhriev@gmail.com>
2024-02-23 14:09:14 +0500

    add aria-labels

--- src/assets/components/header/currency/currency.js
+++ src/assets/components/header/currency/currency.js
@@ -2,12 +2,8 @@ export const toggleCurrency = () => {
 	const currencyToggle = document.querySelector(".currency__toggle")
 	const currencyMenu = document.querySelector(".currency-menu")
 
-	let opened = false
-
 	currencyToggle.addEventListener("click", () => {
 		currencyMenu.classList.toggle("active")
-		currencyToggle.setAttribute("aria-expanded", !opened)
-		opened = !opened
 	})
 }
 
